e3e717a2566125c1c256deef6d522ee8605c839f
hortarion <hrottgers@protonmail.com>
2026-05-13 20:04:44 +0200

    RabbitMQ AMQP + STOMP

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,17 @@
+import amqp from "amqplib";
+
 async function main() {
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
   console.log("Starting Peril server...");
+  if (conn) {
+    console.log("RabbitMQ connected");
+  }
+  process.on("SIGINT", function () {
+    console.log("Peril server shut down with 'ctrl + c'");
+    conn.close();
+    process.exit();
+  });
 }
 
 main().catch((err) => {
